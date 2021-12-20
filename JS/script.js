13a0dfc092e4bf0031cf71d6df01077386652f7d
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2021-12-20 15:33:05 +0100

    added all const

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,29 @@
 console.log('JS OK');
+
+/* 
+1-domando nome all'utente e salvo il nome in una variabile
+    *mando in conosole variabile nome
+
+2-domando cognome all'utente e salvo il cognome in una variabile
+    *mando in conosole variabile cognome
+
+3-domando colore preferito all'utente e salvo il colore preferito in una variabile
+    *mando in console variabile colore preferito
+
+4- raccolgo variabili e scrivo risultato in pagina
+*/
+
+
+
+//1-domando nome all'utente e salvo il nome in una variabile
+const nomeUtente = prompt("Qual'è il tuo nome?");
+    console.log(nomeUtente);
+
+//2-domando cognome all'utente e salvo il nome in una variabile
+const cognomeUtente = prompt("Qual'è il tuo cognome?");
+    console.log(cognomeUtente);
+
+//3-domando colore preferito all'utente e salvo il colore preferito in una variabile
+const colorePreferitoUtente = prompt("Qual'è il tuo colore preferito?");
+    console.log(colorePreferitoUtente);
+
